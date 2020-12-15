8941d6ba1835a6371f408f14406726e0fefae70a
TunTuun <timur.beast@gmail.com>
2020-12-15 19:00:43 +0300

    audio

--- src/modules/functions/stone-functions/create-stones.js
+++ src/modules/functions/stone-functions/create-stones.js
@@ -8,11 +8,22 @@ export const createStones = () => {
       if (!(field.findStone(String(x), String(y)))) {
         const stone = new Stone(String(x), String(y));
         field.addStone(stone);
-
         const stoneCell = findCell(String(x), String(y));
         const stoneOnField = document.createElement('div');
         stoneOnField.classList.add('stone', 'stone_color_' + stone.type);
         stoneCell.append(stoneOnField);
+        // const start = Date.now();
+        // stoneCell.firstChild.style.width = '0%';
+        // const timer = setInterval(function () {
+        //   const timePassed = Date.now() - start;
+        //   if (parseInt(stoneCell.firstChild.style.width, 10) < 90) {
+        //     stoneCell.firstChild.style.width = (timePassed / 5.555) + '%';
+        //     stoneCell.firstChild.style.height = (timePassed / 5.555) + '%';
+        //   }
+        //   if (timePassed > 500) {
+        //     clearInterval(timer);
+        //   }
+        // }, 20);
       }
     }
   }
